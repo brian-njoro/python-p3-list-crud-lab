37d964971b0ef6b807fbcf0ed6794a4a2615f7e2
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-08-21 13:29:48 +0300

    completed assignment

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,35 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    return  ['brian', 'Njoroge', 19, 'fate']
+
 
 def add_element_to_end_of_list(l, element):
-    return None
+    l.append(element)
+    return l
 
 def add_element_to_start_of_list(l, element):
-    return None
+    l[0] = element
+    return l
 
 def remove_element_from_end_of_list(l):
-    return None
+    l.pop()
+    return l
 
 def remove_element_from_start_of_list(l):
-    return None
+    l.remove(0)
+    return l
 
 def retrieve_first_element_from_list(l):
-    return None
+    first_element = l[0]
+    return first_element
 
 def retrieve_element_from_index(l, index):
-    return None
+    specific_index = l[index]
+    return specific_index
 
 def retrieve_last_element_from_list(l):
-    return None
+    last_element = l[-1]
+    return last_element
+
